75c28c26c86d1664acfdfafa6937db5062328caa
sukurax <1421578048@qq.com>
2016-10-05 14:37:43 +0800

    导入前端代码

---
+++ code/www/js/hw.js
@@ -0,0 +1,12 @@
+/*
+* @Author: sukurax
+* @Date:   2016-09-30 13:43:04
+* @Last Modified by:   sukurax
+* @Last Modified time: 2016-09-30 13:48:00
+*/
+
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
---
+++ code/www/js/login.js
@@ -0,0 +1,75 @@
+"use strict"
+
+// 用户名为空，或者格式不对 -》 提示错误，清空密码框，聚焦到用户名框，并全选用户名
+// 用户名不存在 -》同上
+// 密码错误 -》 提示错误，清空密码框，聚焦密码框
+// 聚焦到密码框，全选密码
+// {"_id" : ObjectId(""),"name":"","pwd":"","type":"","email":"","sex":,"age":,"regist":,"remark":""}
+function login_btn() {
+	var
+		get_username = $("#username").val(),
+		get_password = $("#password").val();
+	if (get_username.length <= 0 || get_password.length <= 0) {
+		alert('输入为空！');
+		return;
+	};
+	// alert(get_username + "	" + get_password);
+	console.log(get_username + "	" + get_password);
+	// $.ajax({
+	// 		url: '/public/api/login',
+	// 		type: 'get',
+	// 		dataType: 'json',
+	// 		data: {
+	// 			name: get_username,
+	// 			pwd: get_password,
+	// 			token: "1234567890123456",
+	// 		},
+	// 	})
+	// 	.done(function() {
+	// 		console.log("success");
+	// 	})
+	// 	.fail(function() {
+	// 		console.log("error");
+	// 	})
+	// 	.always(function() {
+	// 		console.log("complete");
+	// 	});
+	$.get('/public/api/login', {
+		name: get_username,
+		pwd: get_password,
+		token: "1234567890123456",
+	}, function(data) {
+		console.log(data + "	");
+	});
+
+
+}
+
+function register_btn() {
+	var
+		get_username = $("#username").val(),
+		get_password = $("#password").val(),
+		get_password_confirm = $("#passwordConfirm").val(),
+		get_email = $("#email").val(),
+		get_age = $("#age").val(),
+		get_sex = jQuery("#selectSex").val();
+	if (get_username.length <= 0 || get_password.length <= 0 || get_password_confirm <= 0 || get_email <= 0 || get_age <= 0) {
+		alert('输入为空！');
+		return;
+	} else if (get_password_confirm !== get_password) {
+		alert('输入密码不一致！');
+		return;
+	};
+
+	alert(get_username + "	" + get_password + "	" + get_password_confirm + "	" + get_email + "	" + get_age + "	" + get_sex);
+}
+// function OnInput (event) {
+// 	alert ("The new content: " + event.target.value);
+
+// }
+
+// function OnPropChanged (event) {
+// 	if (event.propertyName.toLowerCase () == "value") {
+// 		alert ("The new content: " + event.srcElement.value);
+// 	}
+// }
