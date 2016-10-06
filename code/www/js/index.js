662ce43d25330742a564a6c6328e7e01b07dca72
sukurax <1421578048@qq.com>
2016-10-07 00:21:44 +0800

    聊天界面初始版

---
+++ code/www/js/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: sukurax
+ * @Date:   2016-10-06 14:57:51
+ * @Last Modified by:   sukurax
+ * @Last Modified time: 2016-10-06 14:58:51
+ */
+
+'use strict';
+
+function paperBtn() {
+	alert("点击了paper");
+	return;
+}
+
+function settingBtn() {
+	alert("点击了setting");
+	return;
+}
