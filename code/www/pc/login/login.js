3b8beb1e3e47fc6b460e4d78e878bef6d7def2de
earnit <17764591244@163.com>
2016-10-23 11:45:40 +0800

    合并login.js

--- code/www/pc/login/login.js
+++ code/www/pc/login/login.js
@@ -25,45 +25,17 @@ function check_login(){
 	code[9] = "不存在";
 	code[10] = "数据格式非法";
 
-	$.get('/public/api/login',{
+	$.get('/cyzm6/public/api/login',{
 		name: get_userName,
 		pwd: get_password,
 		token: "1234567890123456",
 	},function(data){
 		console.log(JSON.stringify(data) + "	");
-		if(data.code == 1){
-			window.location.href="code/www/index_md/index.html";
+		if(data.code == -1){
+			window.location.href="../../index_md/index.html";
 		}else{
 			alert(code[data.code+1]);
 		}
-	}, function(data) {
-		// console.log(JSON.stringify(data) + "	");
-		//主界面跳转
-		switch (data.code) {
-			case -1:
-				alert("服务器未知错误");
-				break;
-			case 0:
-				alert("失败");
-				break;
-			case 1:
-				// alert("成功");
-				window.location.href="code/www/index_md/index.html";
-				break;
-			case 2:
-				alert("用户不存在");
-				break;
-			case 3:
-				alert("密码错误");
-				break;
-			case 4:
-				alert("用户名已存在");
-				break;
-			default:
-				alert("未知错误");
-				break;
-				return;
-		};
 	});
 }
 function check_register(){
@@ -91,7 +63,7 @@ function check_register(){
 		alert("两次输入密码不一致！");
 		return;
 	}
-	$.get('/public/api/regist',{
+	$.get('/cyzm6/public/api/regist',{
 		name: get_userName,
 		pwd: get_password,
 		email: get_email,
