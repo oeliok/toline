e07ab89ef40dd84cb0ccb32e7f0297da2036c4a9
sukurax <1421578048@qq.com>
2016-11-21 13:51:56 +0800

    merge

--- code/server/control/socket.js
+++ code/server/control/socket.js
@@ -4,31 +4,33 @@
 var si = require('socket.io');
 var ObjectId = require('mongodb').ObjectID;
 
-var log = require('../../log');
-var mongo = require('../../lib/mongo');
-var redis = require('../../lib/redis').redis;
+var log = require('../log');
+var mongo = require('../lib/mongo');
+var redis = require('../lib/redis').redis;
 
 var msgs = {
-    welcome:'你好，欢迎使用！',
-    userExist:'用户信息不存在！',
-    error:'服务器发生错误！'
+    welcome: '你好，欢迎使用！',
+    userExist: '用户信息不存在！',
+    error: '服务器发生错误！'
 };
 
 exports.listen = function listen(server) {
     var io = si.listen(server);
     log.info("socket.io启动服务");
     //监听用户的连接
-    io.on('connection',function (socket){
-        log.debug(socket.id+' join the server!');
-        systemInfoMsg(socket,msgs.welcome);
-        userAuth(io, socket, function (user) {
+    io.on('connection', function(socket) {
+        log.debug(socket.id + ' join the server!');
+        systemInfoMsg(socket, msgs.welcome);
+        userAuth(io, socket, function(user) {
             offlineMessage(socket, user.user._id, user.user.login);
             friendOffline(io, socket, user.user._id);
             friendOnline(io, socket, user.user._id);
             friendMessage(io, socket, user.user._id);
             groupMessage(io, socket, user.user._id);
-            historymsg(socket,user.user._id);
-            socket.emit('auth-s',{"code":1});
+            historymsg(socket, user.user._id);
+            socket.emit('auth-s', {
+                "code": 1
+            });
         });
     });
 };
@@ -36,17 +38,23 @@ exports.listen = function listen(server) {
 
 //系统发送一些提示消息
 function systemInfoMsg(socket, msg) {
-    socket.emit("news",{info:"info",msg:msg});
+    socket.emit("news", {
+        info: "info",
+        msg: msg
+    });
 }
 
 //系统发送一些错误消息
-function systemErrorMsg(socket,msg) {
-    socket.emit("news",{info:"error",msg:msg});
+function systemErrorMsg(socket, msg) {
+    socket.emit("news", {
+        info: "error",
+        msg: msg
+    });
 }
 
 //用户请求与某个人或者群的消息在某个时间点的前面n条数据
-function historymsg(socket,userid) {
-    socket.on('chistory',function (data) {
+function historymsg(socket, userid) {
+    socket.on('chistory', function(data) {
         if (data.type == "group") {
             groupHistoryMsg(socket, userid, data.to, data.date, data.len, data.id);
         } else if (data.type == "friend") {
@@ -58,8 +66,11 @@ function historymsg(socket,userid) {
 }
 
 function groupHistoryMsg(socket, userid, grid, datetime, limit, id) {
-    mongo.getConnection(function (db) {
-        db.collection('guser').find({uid:ObjectId(userid),gid:ObjectId(grid)}).toArray(function (err, data) {
+    mongo.getConnection(function(db) {
+        db.collection('guser').find({
+            uid: ObjectId(userid),
+            gid: ObjectId(grid)
+        }).toArray(function(err, data) {
             if (err) {
                 log.error(err);
                 return false;
@@ -68,31 +79,65 @@ function groupHistoryMsg(socket, userid, grid, datetime, limit, id) {
                 log.debug(data);
                 return false;
             }
-            db.collection('glog').find({"gid":ObjectId(grid),datetime:{$lt:datetime}}).limit(limit).toArray(function (err, glogs) {
+            db.collection('glog').find({
+                "gid": ObjectId(grid),
+                datetime: {
+                    $lt: datetime
+                }
+            }).limit(limit).toArray(function(err, glogs) {
                 if (err) {
                     log.error(err);
                     return false;
                 }
-                socket.emit('shistory',{id:id,from:grid,to:userid,types:'group',sendDate:Date.now(),data:glogs});
+                socket.emit('shistory', {
+                    id: id,
+                    from: grid,
+                    to: userid,
+                    types: 'group',
+                    sendDate: Date.now(),
+                    data: glogs
+                });
             })
         })
     })
 }
 
 function friendHsitoryMsg(socket, userid, frid, datetime, limit, id) {
-    mongo.getConnection(function (db) {
-        var query = {$or:[{myid:ObjectId(userid),frid:ObjectId(frid)},{myid:ObjectId(frid),frid:ObjectId(userid)}]};
-        db.collection('friend').find(query).limit(limit).toArray(function (err, friends) {
+    mongo.getConnection(function(db) {
+        var query = {
+            $or: [{
+                myid: ObjectId(userid),
+                frid: ObjectId(frid)
+            }, {
+                myid: ObjectId(frid),
+                frid: ObjectId(userid)
+            }]
+        };
+        db.collection('friend').find(query).limit(limit).toArray(function(err, friends) {
             var fid = [];
             for (var i in friends) {
-                fid[i] = {fid:friends[i]._id};
+                fid[i] = {
+                    fid: friends[i]._id
+                };
             }
-            db.collection('flog').find({$or:fid,datetime:{$lt:datetime}}).limit(limit).toArray(function (err, flogs) {
+            db.collection('flog').find({
+                $or: fid,
+                datetime: {
+                    $lt: datetime
+                }
+            }).limit(limit).toArray(function(err, flogs) {
                 if (err) {
                     log.error(err);
                     return false;
                 }
-                socket.emit('shistory',{id:id,from:frid,to:userid,types:'friend',sendDate:Date.now(),data:flogs});
+                socket.emit('shistory', {
+                    id: id,
+                    from: frid,
+                    to: userid,
+                    types: 'friend',
+                    sendDate: Date.now(),
+                    data: flogs
+                });
             })
         })
     })
@@ -100,18 +145,22 @@ function friendHsitoryMsg(socket, userid, frid, datetime, limit, id) {
 
 
 function userAuth(io, socket, next) {
-    socket.on('auth-c',function (data) {
-        redis.get('sess:'+data.sessionid,function (err, reply) {
+    socket.on('auth-c', function(data) {
+        redis.get('sess:' + data.sessionid, function(err, reply) {
             if (err) {
                 log.error(err);
-                systemErrorMsg(socket,msgs.error);
-                socket.emit('auth-s',{"code":-1});
+                systemErrorMsg(socket, msgs.error);
+                socket.emit('auth-s', {
+                    "code": -1
+                });
                 return false;
             }
             if (!reply) {
                 log.debug(reply);
-                systemInfoMsg(socket,msgs.userExist);
-                socket.emit('auth-s',{"code":0});
+                systemInfoMsg(socket, msgs.userExist);
+                socket.emit('auth-s', {
+                    "code": 0
+                });
                 return false;
             }
             var user = JSON.parse(reply);
@@ -121,24 +170,35 @@ function userAuth(io, socket, next) {
 }
 
 function offlineMessage(socket, userid, datetime) {
-    socket.on('caoff',function (data) {
+    socket.on('caoff', function(data) {
         if ((typeof data.from) != 'string' || data.from != userid) {
             log.info(data);
-            systemInfoMsg(socket,msgs.userExist);
+            systemInfoMsg(socket, msgs.userExist);
             return false;
         }
-        userFriendOfflineLogs(userid, datetime, function (flogs) {
-            userGroupOfflineLogd(userid, datetime, function (glogs) {
-                socket.emit('saoff',{from:"off-line",to:userid,flogs:flogs,glogs:glogs});
+        userFriendOfflineLogs(userid, datetime, function(flogs) {
+            userGroupOfflineLogd(userid, datetime, function(glogs) {
+                socket.emit('saoff', {
+                    from: "off-line",
+                    to: userid,
+                    flogs: flogs,
+                    glogs: glogs
+                });
             })
         })
     })
 }
 
 function userFriendOfflineLogs(userid, datetime, next) {
-    mongo.getConnection(function (db) {
-        var qery = {$or:[{myid:ObjectId(userid)},{frid:ObjectId(userid)}]};
-        db.collection('friend').find(qery).toArray(function (err, friends) {
+    mongo.getConnection(function(db) {
+        var qery = {
+            $or: [{
+                myid: ObjectId(userid)
+            }, {
+                frid: ObjectId(userid)
+            }]
+        };
+        db.collection('friend').find(qery).toArray(function(err, friends) {
             if (err) {
                 log.error(err);
                 return false;
@@ -149,9 +209,16 @@ function userFriendOfflineLogs(userid, datetime, next) {
             }
             var fid = [];
             for (var i in friends) {
-                fid[i] = {fid:friends[i]._id};
+                fid[i] = {
+                    fid: friends[i]._id
+                };
             }
-            db.collection('flog').find({$or:fid,datetime:{$gte:datetime}}).toArray(function (err, flogs) {
+            db.collection('flog').find({
+                $or: fid,
+                datetime: {
+                    $gte: datetime
+                }
+            }).toArray(function(err, flogs) {
                 if (err) {
                     log.error(err);
                     return false;
@@ -166,8 +233,10 @@ function userFriendOfflineLogs(userid, datetime, next) {
 }
 
 function userGroupOfflineLogd(userid, datetime, next) {
-    mongo.getConnection(function (db) {
-        db.collection('guser').find({uid:ObjectId(userid)}).toArray(function (err, groups) {
+    mongo.getConnection(function(db) {
+        db.collection('guser').find({
+            uid: ObjectId(userid)
+        }).toArray(function(err, groups) {
             if (err) {
                 log.error(err);
                 return false;
@@ -177,10 +246,17 @@ function userGroupOfflineLogd(userid, datetime, next) {
                 return false;
             }
             var gid = [];
-            for (var i in  groups) {
-                gid[i] = {gid:groups[i].gid};
+            for (var i in groups) {
+                gid[i] = {
+                    gid: groups[i].gid
+                };
             }
-            db.collection('glog').find({$or:gid,datetime:{$gte:datetime}}).toArray(function (err, glogs) {
+            db.collection('glog').find({
+                $or: gid,
+                datetime: {
+                    $gte: datetime
+                }
+            }).toArray(function(err, glogs) {
                 if (err) {
                     log.error(err);
                     return false;
@@ -195,26 +271,35 @@ function userGroupOfflineLogd(userid, datetime, next) {
 }
 
 function friendOnline(io, socket, userid) {
-    userGetFriends(userid, function (friends) {
+    userGetFriends(userid, function(friends) {
         for (var i in friends) {
-            useridToSocketid(friends[i]._d, function (userID) {
+            useridToSocketid(friends[i]._d, function(userID) {
                 try {
-                    io.sockets.sockets[reply].emit('sfonline',{"Date":Date.now(),"id":userid})
+                    io.sockets.sockets[reply].emit('sfonline', {
+                        "Date": Date.now(),
+                        "id": userid
+                    })
                 } catch (e) {
                     log.fatal(e);
                 }
             })
         }
     });
-    redis.set('f'+userid, socket.id, function (err, info) {
+    redis.set('f' + userid, socket.id, function(err, info) {
         if (err) {
             log.error(err);
         } else {
             log.debug(info);
         }
     });
-    mongo.getConnection(function (db) {
-        db.collection('user').updateOne({_id:ObjectId(userid)},{$set:{socket:true}}, function (err, info) {
+    mongo.getConnection(function(db) {
+        db.collection('user').updateOne({
+            _id: ObjectId(userid)
+        }, {
+            $set: {
+                socket: true
+            }
+        }, function(err, info) {
             if (err) {
                 log.error(err);
             } else {
@@ -225,8 +310,10 @@ function friendOnline(io, socket, userid) {
 }
 
 function userGetFriends(userid, next) {
-    mongo.getConnection(function (db) {
-        db.collection('friend').find({frid:ObjectId(userid)}).toArray(function (err, friend) {
+    mongo.getConnection(function(db) {
+        db.collection('friend').find({
+            frid: ObjectId(userid)
+        }).toArray(function(err, friend) {
             if (err) {
                 log.error(err);
                 return false;
@@ -237,9 +324,14 @@ function userGetFriends(userid, next) {
             }
             var fid = [];
             for (var i in friend) {
-                fid[i] = {_id:friend[i].myid};
+                fid[i] = {
+                    _id: friend[i].myid
+                };
             }
-            db.collection('user').find({$or:fid,login:true}).toArray(function (err, users) {
+            db.collection('user').find({
+                $or: fid,
+                login: true
+            }).toArray(function(err, users) {
                 if (err) {
                     log.error(err);
                     return false;
@@ -254,8 +346,8 @@ function userGetFriends(userid, next) {
     })
 }
 
-function useridToSocketid(userid,next) {
-    redis.get('f'+userid, function (err , reply) {
+function useridToSocketid(userid, next) {
+    redis.get('f' + userid, function(err, reply) {
         if (err) {
             log.error(err);
             return false;
@@ -265,28 +357,37 @@ function useridToSocketid(userid,next) {
 }
 
 function friendOffline(io, socket, userid) {
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function() {
         log.debug(socket.id + " left room !");
-        userGetFriends(userid, function (friends) {
+        userGetFriends(userid, function(friends) {
             for (var i in friends) {
-                useridToSocketid(friends[i]._d, function (userID) {
+                useridToSocketid(friends[i]._d, function(userID) {
                     try {
-                        io.sockets.sockets[reply].emit('sfoffline',{"Date":Date.now(),"id":userid})
+                        io.sockets.sockets[reply].emit('sfoffline', {
+                            "Date": Date.now(),
+                            "id": userid
+                        })
                     } catch (e) {
                         log.fatal(e);
                     }
                 })
             }
         });
-        redis.del('f'+userid, socket.id, function (err, info) {
+        redis.del('f' + userid, socket.id, function(err, info) {
             if (err) {
                 log.error(err);
             } else {
                 log.debug(info);
             }
         });
-        mongo.getConnection(function (db) {
-            db.collection('user').updateOne({_id:ObjectId(userid)},{$set:{socket:false}}, function (err, info) {
+        mongo.getConnection(function(db) {
+            db.collection('user').updateOne({
+                _id: ObjectId(userid)
+            }, {
+                $set: {
+                    socket: false
+                }
+            }, function(err, info) {
                 if (err) {
                     log.error(err);
                 } else {
@@ -298,7 +399,7 @@ function friendOffline(io, socket, userid) {
 }
 
 function friendMessage(io, socket, userid) {
-    socket.on('cfmsg',function (data) {
+    socket.on('cfmsg', function(data) {
         if ((typeof data.from) != 'string' || data.from != userid) {
             log.info(data);
             return false;
@@ -308,21 +409,28 @@ function friendMessage(io, socket, userid) {
             return;
         }
         data.sendDate = Date.now();
-        useridToSocketid(data.to,function (reply) {
+        useridToSocketid(data.to, function(reply) {
             if (reply) {
-                io.sockets.sockets[reply].emit('sfmsg',data);
+                io.sockets.sockets[reply].emit('sfmsg', data);
             }
-            socket.emit('sfmsg',data);
+            socket.emit('sfmsg', data);
         });
-        mongo.getConnection(function (db) {
-            db.collection('friend').findOne({myid:ObjectId(userid),frid:ObjectId(data.to)},function (err, friend) {
-                if (err){
+        mongo.getConnection(function(db) {
+            db.collection('friend').findOne({
+                myid: ObjectId(userid),
+                frid: ObjectId(data.to)
+            }, function(err, friend) {
+                if (err) {
                     log.error(err);
                     return false;
                 }
                 if (friend) {
-                    db.collection('flog').insertOne({fid:friend_id,msg:data.msg,datetime:data.sendDate}, function (err, reply) {
-                        if (err){
+                    db.collection('flog').insertOne({
+                        fid: friend_id,
+                        msg: data.msg,
+                        datetime: data.sendDate
+                    }, function(err, reply) {
+                        if (err) {
                             log.error(err);
                         } else {
                             log.debug(reply);
@@ -336,18 +444,18 @@ function friendMessage(io, socket, userid) {
 }
 
 function groupMessage(io, socket, userid) {
-    socket.on('cgmsg',function (data) {
+    socket.on('cgmsg', function(data) {
         if ((typeof data.from) != 'string' || data.from != userid) {
             log.debug(data);
             return false;
         }
         if (data.to) {
             data.sendDate = Date.now();
-            checkGroupMenber(userid, data.to,function (gusers) {
+            checkGroupMenber(userid, data.to, function(gusers) {
                 for (var i in gusers) {
-                    useridToSocketid(gusers[i], function (socketid) {
+                    useridToSocketid(gusers[i], function(socketid) {
                         if (io.sockets.sockets[socketid]) {
-                            io.sockets.sockets[socketid].emit('sgmsg',data);
+                            io.sockets.sockets[socketid].emit('sgmsg', data);
                         }
                     })
                 }
@@ -357,13 +465,13 @@ function groupMessage(io, socket, userid) {
 }
 
 function checkGroupMenber(userid, groupid, next) {
-    redis.sismember('g'+groupid,userid, function (err, reply) {
+    redis.sismember('g' + groupid, userid, function(err, reply) {
         if (err) {
             log.error(err);
             return false;
         }
         if (reply) {
-            redis.smembers('g'+groupid,function (err, gusers) {
+            redis.smembers('g' + groupid, function(err, gusers) {
                 if (err) {
                     log.error(err);
                     return false;
@@ -374,8 +482,10 @@ function checkGroupMenber(userid, groupid, next) {
             });
             return false;
         }
-        mongo.getConnection(function (db) {
-            db.collection('guser').find({gid:ObjectId(groupid)}).toArray(function (err, gusers) {
+        mongo.getConnection(function(db) {
+            db.collection('guser').find({
+                gid: ObjectId(groupid)
+            }).toArray(function(err, gusers) {
                 if (err) {
                     log.error(err);
                     return false;
