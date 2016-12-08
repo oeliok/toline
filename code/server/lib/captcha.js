0edf0c7d2b0e99fc234ba4ce701237b21d1550cb
oeli <1028236806@qq.com>
2016-12-08 19:43:42 +0800

    更改验证码实现方式

--- code/server/lib/captcha.js
+++ code/server/lib/captcha.js
@@ -8,14 +8,14 @@ function yzm(req, res, len) {
     var strs = randomtxt(len);
     log.debug(strs);
     req.session.yzm = strs;
-    var canvas = new Canvas(100, 50);
+    var canvas = new Canvas(100, 40);
     var ctx = canvas.getContext('2d');
-    ctx.rect(0,0,100,35);
+    ctx.rect(0,0,100,40);
     ctx.fillStyle="#FFFFFF";
     ctx.fill();
     ctx.fillStyle="#000000";
     ctx.font = '30px Impact';
-    ctx.fillText(strs, 50, 100);
+    ctx.fillText(strs, 0,30);
     var str = canvas.toDataURL().split(',');
     var buf = new Buffer(str[1], 'base64');
     res.send(buf);
