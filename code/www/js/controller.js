cbfab3f766fbe0c0b5b773e13413e2a4c150c441
APPLE <apple@APPLEdeMacBook-Air.local>
2016-12-12 15:51:19 +0800

    好友／群正常聊天

--- code/www/js/controller.js
+++ code/www/js/controller.js
@@ -6,81 +6,119 @@
 //全部好友
 var myfriends ;
 var indexOfFriends = 0;
-var app = angular.module('myApp',['ngRoute']);
+var app = angular.module('myApp',['ngRoute',"ui.router"]);
 //var app = angular.module('myApp',['ui.router']);
-
+socket= io.connect();
 app.controller('chatPubCtrl',function($scope){
     inputName();
 });
 
 app.controller('chatToFrCtrl',function($scope,$route){
     $scope.$route = $route;
-    $scope.list_name = myfriends[indexOfFriends].name;
-    $scoope.back = function(){
+
+    $scope.back = function(){
       history.back();
     };
-    var chatOtherId = myfriends[indexOfFriends]._id;
-    var chatOtherName = myfriends[indexOfFriends].name;
-    if(localStorage.getItem("chatIfo_"+localStorage.currentId+"_"+chatOtherId)){
-        var data=JSON.parse(localStorage.getItem("chatIfo_"+localStorage.currentId+"_"+chatOtherId));
-        console.log(localStorage.getItem("chatIfo_"+localStorage.currentId+"_"+chatOtherId));
-        $scope.dialog_content = "";
-        for(var i=0;i<data.length;i++){
-            if (data[i].sendId === localStorage.currentId) {
-                var dataTemp={
-                    from:"",
-                    name:"",
-                    date:"",
-                    msg:""
-                };
-                var personIfo = localStorage.getItem("personIfo_"+localStorage. currentId);
-                personIfo = JSON.parse(personIfo);
-                dataTemp.name=personIfo.name;
-                dataTemp.from=personIfo._id;
-                var date = new Date(data[i].datetime);
-                var Y = date.getFullYear() + '-';
-                var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-                var D = date.getDate() + ' ';
-                var h = date.getHours() + ':';
-                var m = date.getMinutes()+" ";
-                // var s = date.getSeconds();
-                dataTemp.date=Y+M+D+h+m;
-                dataTemp.msg=data[i].msg;
-                //var html = template('mysay',dataTemp);
-                //contentInput.innerHTML += html;
-
-            } else {
-                var dataTemp={
-                    from:"",
-                    name:"",
-                    date:"",
-                    msg:""
-                };
-                dataTemp.name=chatOtherName;
-                dataTemp.from=chatOtherId;
-                var date = new Date(data[i].datetime);
-                var Y = date.getFullYear() + '-';
-                var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-                var D = date.getDate() + ' ';
-                var h = date.getHours() + ':';
-                var m = date.getMinutes();
-                // var s = date.getSeconds();
-                dataTemp.date=Y+M+D+h+m;
-                dataTemp.msg=data[i].msg;
-                //var html = template('othersay',dataTemp);
-                //contentInput.innerHTML += html;
-                $scope.chat_content = dataTemp ;
+    var contentInput=document.getElementById('contentInput');
+    var chatOtherId=sessionStorage.getItem("chatOtherId");
+    var chatOtherName=sessionStorage.getItem("chatOtherName");
+    var friendorgroup = sessionStorage.getItem("friendorgroup");
+    var dataTemp={
+        from:"",
+        name:"",
+        date:"",
+        msg:""
+    };
+    $scope.list_name = chatOtherName;
+    var personIfo = localStorage.getItem("personIfo_"+localStorage. currentId);
+    personIfo = JSON.parse(personIfo);
+    if("friend"===friendorgroup){
+        if(localStorage.getItem("friendChatIfo_"+localStorage.currentId+"_"+chatOtherId)){
+            var data=JSON.parse(localStorage.getItem("friendChatIfo_"+localStorage.currentId+"_"+chatOtherId));
+            console.log(localStorage.getItem("friendChatIfo_"+localStorage.currentId+"_"+chatOtherId));
+            $("#contentInput").text="";
+            for(var i=0;i<data.length;i++){
+                if (data[i].sendId === localStorage.currentId) {
+                    dataTemp.name=personIfo.name;
+                    dataTemp.from=personIfo._id;
+                    var date = new Date(data[i].datetime);
+                    var Y = date.getFullYear() + '-';
+                    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+                    var D = date.getDate() + ' ';
+                    var h = date.getHours() + ':';
+                    var m = date.getMinutes()+" ";
+                    // var s = date.getSeconds();
+                    dataTemp.date=Y+M+D+h+m;
+                    dataTemp.msg=data[i].msg;
+                    var html = template('mysay',dataTemp);
+                    contentInput.innerHTML += html;
+                } else {
+                    dataTemp.name=chatOtherName;
+                    dataTemp.from=chatOtherId;
+                    var date = new Date(data[i].datetime);
+                    var Y = date.getFullYear() + '-';
+                    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+                    var D = date.getDate() + ' ';
+                    var h = date.getHours() + ':';
+                    var m = date.getMinutes();
+                    // var s = date.getSeconds();
+                    dataTemp.date=Y+M+D+h+m;
+                    dataTemp.msg=data[i].msg;
+                    var html = template('othersay',dataTemp);
+                    contentInput.innerHTML += html;
+                }
             }
         }
-        //contentInput.scrollTop = contentInput.scrollHeight;
-        $scope.chat_content.scrollTop = $scope.chat_content.scrollHeight;
-
     }
+    if("group"===friendorgroup){
+        if(localStorage.getItem("groupChatIfo_"+localStorage.currentId+"_"+chatOtherId)){
+            var data=JSON.parse(localStorage.getItem("groupChatIfo_"+localStorage.currentId+"_"+chatOtherId));
+            console.log(localStorage.getItem("groupChatIfo_"+localStorage.currentId+"_"+chatOtherId));
+            $("#contentInput").text="";
+            for(var i=0;i<data.length;i++){
+                if (data[i].uid === localStorage.currentId) {
+                    dataTemp.name=personIfo.name;
+                    dataTemp.from=personIfo._id;
+                    var date = new Date(data[i].datetime);
+                    var Y = date.getFullYear() + '-';
+                    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+                    var D = date.getDate() + ' ';
+                    var h = date.getHours() + ':';
+                    var m = date.getMinutes()+" ";
+                    // var s = date.getSeconds();
+                    dataTemp.date=Y+M+D+h+m;
+                    dataTemp.msg=data[i].msg;
+                    var html = template('mysay',dataTemp);
+                    contentInput.innerHTML += html;
+                } else {
+                    dataTemp.name=data[i].name;
+                    dataTemp.from=data[i].uid;
+                    var date = new Date(data[i].datetime);
+                    var Y = date.getFullYear() + '-';
+                    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+                    var D = date.getDate() + ' ';
+                    var h = date.getHours() + ':';
+                    var m = date.getMinutes();
+                    // var s = date.getSeconds();
+                    dataTemp.date=Y+M+D+h+m;
+                    dataTemp.msg=data[i].msg;
+                    var html = template('othersay',dataTemp);
+                    contentInput.innerHTML += html;
+                }
+            }
+        }
+    }
+    contentInput.scrollTop = contentInput.scrollHeight;
 
     $scope.say=function () {
-        if ($scope.dialog_content != ''){
-            console.log("sendmsg:" + $scope.dialog_content);
-            socketSendChatmsg(chatOtherId,$scope.dialog_content);
+        if (document.getElementById('msg').value != ''){
+            console.log("sendmsg:"+document.getElementById('msg').value);
+            if("friend"===friendorgroup){
+                socketSendChatmsg(chatOtherId,document.getElementById('msg').value);
+            }
+            if("group"===friendorgroup){
+                socketSendGroupmsg(chatOtherId,document.getElementById('msg').value);
+            }
         } else
             Materialize.toast('Nothing input,QAQ', 2500, 'rounded');
     }
@@ -88,6 +126,37 @@ app.controller('chatToFrCtrl',function($scope,$route){
 });
 app.controller('dialogCtrl',function($scope,$route){
     $scope.$route = $route;
+    getCurrentId();
+    getSesssionId();
+
+    socketMonitor();
+    socketConfirm();
+    if(localStorage.getItem("personIfo_"+localStorage. currentId)){
+        var personIfo = localStorage.getItem("personIfo_"+localStorage. currentId);
+        personIfo = JSON.parse(personIfo);
+        //***对应名字，图片，提示信息
+        $scope.ownerName=personIfo.name;
+        $scope.owenerPic=personIfo._id;
+        $scope.promptText="主界面";
+        if(localStorage.getItem("chatIfo_"+localStorage. currentId)){
+            //遍历好友列表，获取好友聊天信息
+            var chatIfo = localStorage.getItem("chatIfo_"+localStorage. currentId);
+            chatIfo = JSON.parse(chatIfo);
+            for (var i=0;i<chatIfo.length;i++)
+            {
+                socketHistoryGet("friend",chatIfo[i]._id,100);
+            }
+        }
+        if(localStorage.getItem("groupIfo_"+localStorage. currentId)){
+            //遍历群列表，获取群聊天信息
+            var groupChatIfo = localStorage.getItem("groupIfo_"+localStorage. currentId);
+            groupChatIfo = JSON.parse(groupChatIfo);
+            for (var i=0;i<groupChatIfo.length;i++)
+            {
+                socketHistoryGet("group",groupChatIfo[i]._id,100);
+            }
+        }
+    }
 });
 app.controller('person_info',function($scope){
     getCurrentId();
@@ -97,14 +166,17 @@ app.controller('person_info',function($scope){
 });
 app.controller('group',function($scope,$route){
     $scope.$route = $route;
-    $scope.back = function (){
-        //location.href = "frlistCtrl";
-        history.back();
+    $scope.list_name = "群列表";
+    var group = localStorage.getItem("groupIfo_"+localStorage. currentId);
+    console.log(group);
+    $scope.list = JSON.parse(group);
+    $scope.index = function (x) {
+        sessionStorage.setItem("chatOtherId",group[x]._id);
+        sessionStorage.setItem("chatOtherName",group[x].name);
+        sessionStorage.setItem("friendorgroup","group");
+        console.log("当前groupList"+ x +"："+" otherid:"+group[x]._id+" othername:"+group[x].name);
+        $scope.$apply();
     };
-    $scope.list = [
-        {name:"康德软件"},
-        {name:"青马"}
-    ];
 });
 app.controller('search',function($scope,$route){
     $scope.$route = $route;
@@ -125,64 +197,18 @@ app.controller('frListCtrl',function ($scope,$route) {
     $scope.$route = $route;
     $scope.list_name = "好友列表";
     //初始化
-    getCurrentId();
-    getSesssionId();
-    socket= io.connect();
-    socketMonitor();
-    socketConfirm();
-    //if(localStorage.getItem("personIfo_"+localStorage. currentId)){
-    //    var personIfo = localStorage.getItem("personIfo_"+localStorage. currentId);
-    //    personIfo = JSON.parse(personIfo);
-    //    //***对应名字，图片，提示信息
-    //    $scope.ownerName=personIfo.name;
-    //    $scope.owenerPic=personIfo._id;
-    //    $scope.promptText="主界面";
-    //    //遍历好友列表，获取好友聊天信息
-    //    var chatIfo = localStorage.getItem("chatIfo_"+localStorage. currentId);
-    //    chatIfo = JSON.parse(chatIfo);
-    //    for (var i=0;i<chatIfo.length;i++)
-    //    {
-    //        socketHistoryGet("friend",chatIfo[i]._id,100);
-    //    }
-    //}
-
-
-    //var content=document.getElementById('content');
-    //
-    //var chatIfo = localStorage.getItem("chatIfo_"+localStorage. currentId);
-    //chatIfo = JSON.parse(chatIfo);
-    //for (var i=0;i<chatIfo.length;i++)
-    //{
-    //    var html = template('chatList', chatIfo[i]);
-    //    content.innerHTML += html;
-    //};
-    //$("#content a").click(function(){
-    //    pos = $("#content a").index($(this));
-    //    var chatOtherId=(JSON.parse( localStorage.getItem("chatIfo_"+localStorage. currentId))[pos])._id;
-    //    var chatOtherName=(JSON.parse( localStorage.getItem("chatIfo_"+localStorage. currentId))[pos]).name;
-    //    sessionStorage.setItem("chatOtherId",chatOtherId);
-    //    sessionStorage.setItem("chatOtherName",chatOtherName);
-    //    console.log("FriendList pos:"+pos+" otherid:"+chatOtherId+" othername:"+chatOtherName);
-    //    //var pos = $(this).index();
-    //    //双人聊天
-    //
-    //    $location.path('/chat');
-    //    $scope.$apply();
-    //
-    //});
-
-
-    getCurrentId();
-    loadFriendList();
     var friends = JSON.parse( localStorage.getItem("chatIfo_"+localStorage. currentId) );
     myfriends = friends;
     $scope.list = friends;
-    console.log("friends_list：" + friends[0].name);
-    console.log("friends_list：" + friends[0].remark);
     console.log(JSON.stringify(friends));
     //记录当前聊天好友的li值
     $scope.index = function (x) {
         indexOfFriends = x;
+        sessionStorage.setItem("chatOtherId",myfriends[x]._id);
+        sessionStorage.setItem("chatOtherName",myfriends[x].name);
+        sessionStorage.setItem("friendorgroup","friend");
+        console.log("当前聊天对象："+" otherid:"+myfriends[x]._id+" othername:"+myfriends[x].name);
+        $scope.$apply();
     };
 
 
@@ -198,8 +224,7 @@ app.config(function ($routeProvider) {
     //when('/friends_list',{
     //    templateUrl:'friends_list.html',
     //    controller:'frListCtrl'
-    //}).
-        //好友列表子菜单 好友聊天界面
+    //})
     when('/dialog',{
         templateUrl:'dialog_panel.html',
         controller:'dialogCtrl'
