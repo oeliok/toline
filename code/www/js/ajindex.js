54424d479ddd1ec362b12ea51368781c23d3326b
sukurax <1421578048@qq.com>
2016-12-16 16:22:52 +0800

    修改确认加群/好友重复提示问题

--- code/www/js/ajindex.js
+++ code/www/js/ajindex.js
@@ -81,7 +81,7 @@ app.controller('homeCtrl',function ($scope,$location,$route) {
 					currentChats.splice(pos,1);
 					sessionStorage.setItem("currentChat_"+localStorage.currentId,JSON.stringify(currentChats));
 					$route.reload();
-				}if(temp===11){
+				}else if(temp===11){
 					Materialize.toast("添加"+chatOtherName+"失败_(:зゝ∠)_:"+code[temp+1], 1500, 'rounded');
 					currentChats.splice(pos,1);
 					sessionStorage.setItem("currentChat_"+localStorage.currentId,JSON.stringify(currentChats));
@@ -102,7 +102,7 @@ app.controller('homeCtrl',function ($scope,$location,$route) {
 					currentChats.splice(pos,1);
 					sessionStorage.setItem("currentChat_"+localStorage.currentId,JSON.stringify(currentChats));
 					$route.reload();
-				}if(temp===13){
+				}else if(temp===13){
 					Materialize.toast(msg+",申请失败_(:зゝ∠)_:"+code[temp+1], 1500, 'rounded');
 					currentChats.splice(pos,1);
 					sessionStorage.setItem("currentChat_"+localStorage.currentId,JSON.stringify(currentChats));
